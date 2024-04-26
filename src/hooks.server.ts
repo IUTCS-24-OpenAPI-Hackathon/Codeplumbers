f8072cbd3296ae5ddb48fb84f97687f5c2698a87
saged-sama <mahmudulhasanshajid@gmail.com>
2024-04-27 04:58:24 +0600

    nice

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,6 @@ import { serializeNonPOJOs } from "$lib/utils";
 import PocketBase from "pocketbase";
 
 export const handle = async ({ event, resolve }: {event: any, resolve: any}): Promise<any> => {
-
     let { latitude, longitude }: {
         latitude: number | null,
         longitude: number | null,
@@ -11,13 +10,13 @@ export const handle = async ({ event, resolve }: {event: any, resolve: any}): Pr
         longitude: null
     }
 
-
     event.locals.title = "Hackathon";
     event.locals.pb = new PocketBase("https://hackathonbas.pockethost.io/");
     event.locals.pb.authStore.loadFromCookie(event.request.headers.get("cookie") || "");
 
     if(event.locals.pb.authStore.isValid){
         event.locals.user = serializeNonPOJOs(event.locals.pb.authStore.model);
+        event.locals.location = {latitude, longitude};
     }
     else{
         event.locals.user = undefined;
